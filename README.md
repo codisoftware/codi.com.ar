2632d9fc533620ecc2f829df9b2d520ae19feca2
Diego Vidal <dv3000@gmail.com>
2024-02-26 16:43:45 -0300

    fix: contact form and captcha

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## Checklist
 
 ### TODO
-- [ ] Corregir el funcionamiento del formulario.
+- [x] Corregir el funcionamiento del formulario.
 - [x] Ajustar el color del hover sobre los enlaces "Our Solutions" a `#37a4dc`.
 - [x] Agregar nuevamente el menú superior "Our Solutions" y eliminar los enlaces a las páginas particulares que no funcionan.
-- [ ] Solucionar los problemas con Google reCAPTCHA.
+- [x] Solucionar los problemas con Google reCAPTCHA.
 - [x] Revisar y corregir nuevamente los textos.
 
 ### TODO 2
--- wp-content/plugins/Codi-contact-form/public/assets/js/script.js
+++ wp-content/plugins/Codi-contact-form/public/assets/js/script.js
@@ -1,14 +1,11 @@
 (function ($) {
   $(function () {
-    var result = $(".form-result");
-    var form = document.getElementById("codi-contact");
     var button = $("#submit");
     var requiredFieldMsg = "Required",
       minCharactersMsg = "The minimum of characters is 2",
       incorrectFormatMsg = "Incorrect format";
     var textarea = document.getElementById("message");
-    var recaptcha = document.getElementById("g-recaptcha-response");
-    var SITE_KEY = jsRecaptcha.site_key;
+
 
     $.validator.addMethod("onlyText", function (value, element) {
       return this.optional(element) || /^[A-zÀ-ÖØ-öø-ÿ',. ]+$/.test(value);
@@ -22,17 +19,6 @@
       this.style.height = "auto";
       this.style.height = this.scrollHeight + "px";
     });
-
-    function loadReCaptcha() {
-      grecaptcha.ready(function () {
-        grecaptcha
-          .execute(SITE_KEY, { action: "form_submission" })
-          .then(function (token) {
-            recaptcha.value = token;
-          });
-      });
-    }
-    loadReCaptcha();
 
     $("#codi-contact").validate({
       errorElement: "span",
@@ -62,67 +48,50 @@
       },
       submitHandler: function () {
         button.html("loading...");
-        setTimeout(() => {
-          var dataRecaptcha = {
-            action: "codi_contact_recaptcha",
-            g_recaptcha_response: recaptcha.value,
-          };
-
-          $.ajax({
-            url: jsVars.ajax_url,
-            type: "POST",
-            data: dataRecaptcha,
-            success: function (data) {
-              console.log(data);
-              if (data.status) {
-                var data = {
-                  action: "codi_contact_form",
-                  nonce: jsVars.nonce,
-                  name: $("#name").val(),
-                  email: $("#email").val(),
-                  message: $("#message").val(),
-                };
-                $.ajax({
-                  url: jsVars.ajax_url,
-                  type: "POST",
-                  data: data,
-                  beforeSend: function () {},
-                })
-                  .done(function (data, textStatus, jqXHR) {
-                    if (data.status) {
-                      result.addClass("success");
-                      result.removeClass("error");
-                      result.html(textStatus);
-                      form.reset();
-                    }
-                  })
-                  .fail(function (jqXHR, textStatus, errorThrown) {
-                    result.addClass("error");
-                    result.html(textStatus);
-                    console.log(textStatus);
-                  })
-                  .always(function () {
-                    button.html("Submit");
-                    setTimeout(() => {
-                      result.removeClass("error");
-                      result.removeClass("success");
-                      result.html("");
-                    }, 3000);
-                  });
-              }
-            },
-            error: function (error) {
-              result.addClass("error");
-              result.html(error.responseJSON.data);
-              button.html("Submit");
-              setTimeout(() => {
-                result.removeClass("error");
-                result.html("");
-              }, 3000);
-            },
-          });
-        }, 500);
+        grecaptcha.execute();
       },
     });
   });
 })(jQuery);
+
+function onSubmit(token) {
+  var $ = jQuery
+  console.log('token', token);
+  var result = $(".form-result");
+  var button = $("#submit");
+  var data = {
+    action: "codi_contact_form",
+    recaptcha: token,
+    name: $("#name").val(),
+    email: $("#email").val(),
+    message: $("#message").val(),
+  };
+  $.ajax({
+    crossDomain: true,
+    url: 'https://api.codi.com.ar/send.php',
+    type: "POST",
+    data: data,
+    beforeSend: function () {},
+  })
+      .done(function (data, textStatus, jqXHR) {
+        if (data.status) {
+          result.addClass("success");
+          result.removeClass("error");
+          result.html(textStatus);
+          form.reset();
+        }
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        result.addClass("error");
+        result.html(textStatus);
+        console.log(textStatus);
+      })
+      .always(function () {
+        button.html("Submit");
+        setTimeout(() => {
+          result.removeClass("error");
+          result.removeClass("success");
+          result.html("");
+        }, 3000);
+      });
+}
